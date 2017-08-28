ba44f56014cd590d82b0683411d809daba946dde
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-08-28 14:38:38 +0800

    create admin user and dbuser if provided in env

--- set_mongo_user.sh
+++ set_mongo_user.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 USER=${MONGODB_USER:-"admin"}
-DATABASE=${MONGODB_DATABASE:-"admin"}
 PASS=${MONGODB_PASS:-"admin_password"}
+DATABASE=${MONGODB_DATABASE:-"admin"}
+DBUSER=${DB_USER:-"db_user"}
+DBPWD=${DB_PWD:-"db_pwd"}
 
 RET=1
 while [[ RET -ne 0 ]]; do
@@ -22,18 +24,17 @@ if [ "$DATABASE" != "admin" ]; then
     echo "=> Creating an ${USER} user with a ${_word} password in ${DATABASE}"
     mongo admin -u $USER -p $PASS << EOF
 use $DATABASE
-db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
+db.createUser({user: '$DBUSER', pwd: '$DBPWD', roles:[{role:'dbOwner',db:'$DATABASE'}]})
 EOF
 fi
 
 
 echo "=> Done!"
 touch /data/db/.mongodb_password_set
-
 echo "========================================================================"
 echo "You can now connect to this MongoDB server using:"
 echo ""
-echo "    mongo $DATABASE -u $USER -p $PASS --host <host> --port <port>"
+echo "    mongo -u $USER -p $PASS --host <host> --port <port>"
 echo ""
 echo "========================================================================"
 
